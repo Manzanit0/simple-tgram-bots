8247f03922c5a841d56ede10514f15ec02a0d50b
J. G. Manzano <javier.garcia@s4g.es>
2017-09-19 19:47:58 +0200

    :bug: small RegEx bugfix.

--- src/littleAppleBot/bot.js
+++ src/littleAppleBot/bot.js
@@ -10,7 +10,7 @@ const config = require('./config');
 // Sets all the words that the bot has to reply to.
 const values = config.responseValues;
 for (const property in values) {
-    app.hears(new RegExp('^[^\/\s].*' + property + '$|^' + property + '$', 'i'), ({ reply }) => reply(values[property]));
+    app.hears(new RegExp('^[^\/].*' + property + '$|^' + property + '$', 'i'), ({ reply }) => reply(values[property]));
 }
 
 // Export bot handler
